8853d3ec0295063e3c7b84ead15fb7f8d6f780e5
Taegu <gmdals45@gmail.com>
2023-09-24 16:44:09 +0900

    2023-09-24 update Frontend

--- Application/Frontend(react)/src/component/Header.js
+++ Application/Frontend(react)/src/component/Header.js
@@ -1,9 +1,21 @@
 import "./Header.css"
 
 const Header = () => {
-  return <div className="Header">
-      <h3>오늘은</h3>
-      <h1>{new Date().toDateString()}</h1>
-  </div>
+  return (
+      <div className="Header" >
+
+          <div class="image-with-text" >
+              <h3>오늘은</h3>
+
+              <a href="" className="" class="image-container">
+                  <img src="/img/login.svg" alt="간편로그인" />
+              </a>
+          </div>
+
+          <h1>{new Date().toDateString()}</h1>
+
+      </div>
+
+  )
 };
 export default Header;
