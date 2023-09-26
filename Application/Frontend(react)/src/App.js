20570c81c911a22b266d098681fd98236a80c46d
iamtaegu <gmdals45@gmail.com>
2023-09-26 09:06:59 +0900

    2023-09-26 update memorization

--- Application/Frontend(react)/src/App.js
+++ Application/Frontend(react)/src/App.js
@@ -30,8 +30,6 @@ const mockTodo = [
  * useState 같은 경우에는 컴포넌트 안에 선언 돼 있기 때문에 상태코드를 분리할 수 없음
  * useReducer 훅에 의해서
  *  dispatch 호출 > reducer 호출
- * 최적화
- *  ㅇ useCallback: 불필요한 함수 재생성 방지
  */
 function reducer(state, action) {
     switch (action.type) {
@@ -59,6 +57,10 @@ function App() {
     * todo - mockTodo
     * 컴포넌트에서 상태변화 코드 분리를 위한 리액트 훅 변경
     *   useState -> useReducer
+    * 최적화
+    *   ㅇ useCallback: 불필요한 함수 재생성 방지
+    *       - onCreate에서는 reducer에 최신 state만 제공하기 때문에 불필요
+    *       - onDelete, onUpdate에만 적용
     */
 
     //const [todo, setTodo] = useState(mockTodo);
