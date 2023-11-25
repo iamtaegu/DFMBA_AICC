2a910eaecdff15de38809815612d4c75270400d8
Taegu <gmdals45@gmail.com>
2023-11-25 16:19:50 +0900

    2023-11-25 add OAuth2.0 service

--- Application/Backend(server)/aiccEurekaAuthServer/src/main/java/com/dfmba/aicc/eurekaAuthClient/EurekaAuthClientApplication.java
+++ Application/Backend(server)/aiccEurekaAuthServer/src/main/java/com/dfmba/aicc/eurekaAuthClient/EurekaAuthClientApplication.java
@@ -2,11 +2,8 @@ package com.dfmba.aicc.eurekaAuthClient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
-@RefreshScope
 public class EurekaAuthClientApplication {
 
 	public static void main(String[] args) {
